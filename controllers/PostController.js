3bd5d93c759ba913383fd56dd13e3d7fb4c96788
Gleb Leus <lv.gleb44@gmail.com>
2024-12-19 02:41:12 +0300

    fix: get one post error

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,94 +3,113 @@ import { isValidObjectId } from 'mongoose';
 import PostModel from '../models/Post.js';
 
 export const create = async (req, res) => {
-    try {
-        const doc = new PostModel({
-            title: req.body.title,
-            text: req.body.text,
-            imageUrl: req.body.imageUrl,
-            tags: req.body.tags,
-            user: req.userId
-        });
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
 
-        const post = await doc.save();
+    const post = await doc.save();
 
-        res.json(post);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: 'Не удалось создать статью'
-        });
-    }
-}
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'Не удалось создать статью',
+    });
+  }
+};
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'Не удалось получить теги',
+    });
+  }
+};
 
 export const getAll = async (req, res) => {
-    try {
-        const posts = await PostModel.find().populate('user').exec();
+  try {
+    const posts = await PostModel.find().populate('user').exec();
 
-        res.json(posts);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: 'Не удалось получить статьи'
-        });
-    }
-}
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
 
 export const getOne = async (req, res) => {
-    try {
-        const postId = req.params.id;
+  try {
+    const postId = req.params.id;
 
-        PostModel.findOneAndUpdate({_id: postId}, {$inc: { viewsCount: 1 }},{returnDocument: 'after'})
-            .then(doc => res.json(doc))
-            .catch(err => {
-              console.log(err);
-              res.status(404).json({ message: 'Статья не найдена' });
-            });
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: 'Не удалось получить статьи'
-        });
-    }
-}
+    const doc = await PostModel.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { viewsCount: 1 } },
+      { returnDocument: 'after' },
+    );
+
+    res.json(doc);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
 
 export const remove = async (req, res) => {
-    try {
-        const postId = req.params.id;
+  try {
+    const postId = req.params.id;
 
-        PostModel.findOneAndDelete({_id: postId})
-            .then(doc => res.json({success: true}))
-            .catch(err => res.status(404).json({message: 'Не удалосб удалить статью'}));
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: 'Не удалось получить статьи'
-        });
-    }
-}
+    PostModel.findOneAndDelete({ _id: postId })
+      .then((doc) => res.json({ success: true }))
+      .catch((err) => res.status(404).json({ message: 'Не удалосб удалить статью' }));
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
 
 export const update = async (req, res) => {
-    try {
-        const postId = req.params.id;
+  try {
+    const postId = req.params.id;
 
-        await PostModel.updateOne(
-            {_id: postId}, 
-            {
-                title: req.body.title,
-                text: req.body.text,
-                imageUrl: req.body.imageUrl,
-                tags: req.body.tags,
-                user: req.userId
-            }
-        );
+    await PostModel.updateOne(
+      { _id: postId },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      },
+    );
 
-        res.json({
-            succses: true
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось обновить статью'
-        });
-    }
-}
+    res.json({
+      succses: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось обновить статью',
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-import express from "express";
+import express from 'express';
 import mongoose from 'mongoose';
-import multer from "multer";
+import multer from 'multer';
+import cors from 'cors';
 
-import { loginValidation, postCreateValidation, registerValidation } from "./validations.js";
+import { loginValidation, postCreateValidation, registerValidation } from './validations.js';
 import { checkAuth, handleValidationErrors } from './utils/index.js';
 
-import { UserController, PostController } from "./controllers/index.js";
+import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-    .connect('mongodb+srv://gleb:J0r8uwPkJsRJ7f6A@blog.r1jiuby.mongodb.net/blog?retryWrites=true&w=majority')
-    .then(() => console.log('DB OK'))
-    .catch((err) => console.log('DB ERROR', err));
+  .connect(
+    'mongodb+srv://gleb:J0r8uwPkJsRJ7f6A@blog.r1jiuby.mongodb.net/blog?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB OK'))
+  .catch((err) => console.log('DB ERROR', err));
 
 const app = express(); // создали express приложение
 
 // сохранение загруженных файлов
 const storage = multer.diskStorage({
-    destination: (_, __, callback) => {
-        callback(null, 'uploads');
-    },
-    filename: (_, file, callback) => {
-        callback(null, file.originalname);
-    }
+  destination: (_, __, callback) => {
+    callback(null, 'uploads');
+  },
+  filename: (_, file, callback) => {
+    callback(null, file.originalname);
+  },
 });
 
 // функция для загрузки файлов
-const upload = multer({storage});
+const upload = multer({ storage });
 
 app.use(express.json()); // указываем чтение json формата
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 // получение информации о профиле
@@ -39,10 +43,10 @@ app.post('/auth/login', loginValidation, handleValidationErrors, UserController.
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`
-    })
-})
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
@@ -50,11 +54,12 @@ app.post('/posts', checkAuth, postCreateValidation, registerValidation, PostCont
 app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch('/posts/:id', postCreateValidation, registerValidation, checkAuth, PostController.update);
 
+app.get('/tags', PostController.getLastTags);
 
 app.listen(4444, (err) => {
-    if (err) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log('server ok');
+  console.log('server ok');
 });
